0742fd502187fd592554eb3c9894e973a0a11e1f
Kacpron123 <kacper030904@gmail.com>
2025-03-17 19:14:17 +0100

    added rand

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
-
+#include<cstdlib>
+#include<ctime>
 
 
 
 int main(){
+    srand(time(NULL));
     std::cout<<"print siema"<<std::endl;
     for(int i = 0;i<10;i++){
-        std::cout<<"jajco";
+        std::cout<<rand()%100<<std::endl;
     }
 }
